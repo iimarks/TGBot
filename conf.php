6eb0c10e31d0fc46389e4a6990a58fc44a90dd6d
MARK <37331833+iimarks@users.noreply.github.com>
2018-12-08 01:05:10 +0300

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+
+$TGBot->settings(['disable_web_page_preview' => false,
+'parse_mode'                                 => 'HTML', //can be markdown
+'PostgreSQL'                                 => false,
+'MySQL'                                      => false,
+'adminMySQL'                                 => false, //Use MySQL for global post etc
+'adminPostGreSQL'                            => false, //Use PostGreSQL for global post etc
+'table_name'                                 => 'EasyTGBot',
+'admins'                                     => [
+    685380727,
+],
+]);
+$TGBot->PostgreDBCredentials('localhost', 'USERNAME', 'PASSWORD', 'DATABASE');
+$TGBot->MySQLDBCredentials('localhost', 'USERNAME', 'PASSWORD', 'DATABASE');
